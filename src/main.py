3ff3cdd9e5cf97e97f5111bd3810a0e565ca2657
lucascesconetto <lcesconetto@uolinc.com>
2023-05-27 17:30:38 -0300

    refactor: implementation using dependency container

--- src/main.py
+++ src/main.py
@@ -1,33 +1,26 @@
-from requests import Session
-from src.Driven.Quotation.FakeProvider.FakeAdapter import FakeAdapter
-from src.Driven.Whatsapp.Fake.FakeWhatsappAdapter import FakeWhatsappAdapter
-from src.Config.Environment import Environment
-from src.Driven.Log.LogAdapter import LogAdapter
+from src.Port.WhatsappPort import WhatsappPort
+from src.Port.QuotationProviderPort import QuotationProviderPort
+from src.Command.QuotationCommandHandler import QuotationCommandHandler
+from src.Config.Container import Container
+from src.Config.Dependency import Dependency
 
 def handler(event, context):    
-    session = Session()
-    logger = LogAdapter()
     
-    adapter = FakeAdapter(session=session, logger=logger)
+    container = Container()
+    Dependency.register_default(container=container)
     
-    quotation_result= adapter.collect()
-    quotation_result.filter_by_location('pr')
+    quotation_adapter = QuotationCommandHandler(quotation_port=container.get(QuotationProviderPort))
+    quotation_result= quotation_adapter.execute()
+    # quotation_result.filter_by_location('ro')
     
-    whatsappAdapter = FakeWhatsappAdapter(
-        account_sid=Environment.get('FAKE_ACCESS_ID'),
-        auth_token=Environment.get('FAKE_AUTH_TOKEN'),
-        logger=logger
-    )
+    whatsappAdapter = container.get(key=WhatsappPort)
     
-    # Example send message 
     message = f'*Cotação Boi Gordo* \nData Referência: {quotation_result.first().reference_date.getValue()}'
     whatsappAdapter.send_message(message=message)
 
     for quotation in quotation_result.getValue():
         message = f"*{quotation.location.getValue()}* \nÀ Vista: *R$ {quotation.current.getValue()}*\nÀ Prazo: *R$ {quotation.future.getValue()}*\nÀ Vista Sem Funrural: *R$ {quotation.current_without_funrural.getValue()}*\nÀ Prazo Sem Funrural: *R$ {quotation.future_without_funrural.getValue()}*\nVariação de preço comparado a SP: *{quotation.variation.getValue()}*\n\n"
         whatsappAdapter.send_message(message=message)
-
-
 
 if __name__ == '__main__':
     handler('localhost', None)
